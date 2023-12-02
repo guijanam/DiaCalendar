95930bccd1cc0d9eeb659fd22207e7ed20c2b20f
guijanam <developergui7@gmail.com>
2023-12-02 15:01:52 +0900

    달력까지 완성

--- app/src/main/java/com/example/diacalendar/MainActivity.kt
+++ app/src/main/java/com/example/diacalendar/MainActivity.kt
@@ -5,22 +5,40 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.darkColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,6 +50,24 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.diacalendar.ui.theme.DiaCalendarTheme
+import com.kizitonwose.calendar.compose.CalendarState
+import com.kizitonwose.calendar.compose.ContentHeightMode
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.VerticalCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.OutDateStyle
+import com.kizitonwose.calendar.core.daysOfWeek
+import com.kizitonwose.calendar.core.nextMonth
+import com.kizitonwose.calendar.core.previousMonth
+import com.kizitonwose.calendar.core.yearMonth
+import com.example.diacalendar.shared.displayText
+
+import kotlinx.coroutines.launch
+import java.time.DayOfWeek
+import java.time.LocalDate
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -142,18 +178,199 @@ fun NavigationGraph(navController: NavHostController) {
 
 
 @Composable
-fun CalendarScreen() {
-    Box(
+fun CalendarScreen(horizontal: Boolean = true) {
+    val today = remember { LocalDate.now() }
+    val currentMonth = remember(today) { today.yearMonth }
+    val startMonth = remember { currentMonth.minusMonths(500) }
+    val endMonth = remember { currentMonth.plusMonths(500) }
+    val selections = remember { mutableStateListOf<CalendarDay>() }
+    val daysOfWeek = remember { daysOfWeek() }
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(androidx.compose.material.MaterialTheme.colors.primary)
+            .background(colorResource(id = R.color.red_800))//상단 년도 배경색
+            .padding(top = 5.dp),
     ) {
-        androidx.compose.material.Text(
-            text = stringResource(id = R.string.text_calendar),
-            style = androidx.compose.material.MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.Center)
+        val state = rememberCalendarState(
+            startMonth = startMonth,
+            endMonth = endMonth,
+            firstVisibleMonth = currentMonth,
+            firstDayOfWeek = daysOfWeek.first(),
+            outDateStyle = OutDateStyle.EndOfGrid,
+        )
+        val coroutineScope = rememberCoroutineScope()
+        val visibleMonth = rememberFirstVisibleMonthAfterScroll(state)
+        // Draw light content on dark background.
+        CompositionLocalProvider(LocalContentColor provides darkColors().onSurface) {
+            SimpleCalendarTitle(
+                modifier = Modifier.padding(vertical = 10.dp, horizontal = 8.dp),
+                currentMonth = visibleMonth.yearMonth,
+                goToPrevious = {
+                    coroutineScope.launch {
+                        state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.previousMonth)
+                    }
+                },
+                goToNext = {
+                    coroutineScope.launch {
+                        state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.nextMonth)
+                    }
+                },
+            )
+            FullScreenCalendar(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(id = R.color.blue_grey_700))//달력 배경색
+                    .testTag("Calendar"),
+                state = state,
+                horizontal = horizontal,
+                dayContent = { day ->
+                    Day(
+                        day = day,
+                        isSelected = selections.contains(day),
+                        isToday = day.position == DayPosition.MonthDate && day.date == today,
+                    ) { clicked ->
+                        if (selections.contains(clicked)) {
+                            selections.remove(clicked)
+                        } else {
+                            selections.add(clicked)
+                        }
+                    }
+                },
+                // The month body is only needed for ui test tag.
+                monthBody = { _, content ->
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .testTag("MonthBody"),
+                    ) {
+                        content()
+                    }
+                },
+                monthHeader = {
+                    MonthHeader(daysOfWeek = daysOfWeek)
+                },
+                monthFooter = { month ->
+                    val count = month.weekDays.flatten()
+                        .count { selections.contains(it) }
+                    MonthFooter(selectionCount = count)
+                },
+            )
+        }
+    }
+}
+
+@Composable
+private fun FullScreenCalendar(
+    modifier: Modifier,
+    state: CalendarState,
+    horizontal: Boolean,
+    dayContent: @Composable BoxScope.(CalendarDay) -> Unit,
+    monthHeader: @Composable ColumnScope.(CalendarMonth) -> Unit,
+    monthBody: @Composable ColumnScope.(CalendarMonth, content: @Composable () -> Unit) -> Unit,
+    monthFooter: @Composable ColumnScope.(CalendarMonth) -> Unit,
+) {
+    if (horizontal) {
+        HorizontalCalendar(
+            modifier = modifier,
+            state = state,
+            calendarScrollPaged = true,
+            contentHeightMode = ContentHeightMode.Fill,
+            dayContent = dayContent,
+            monthBody = monthBody,
+            monthHeader = monthHeader,
+            monthFooter = monthFooter,
+        )
+    } else {
+        VerticalCalendar(
+            modifier = modifier,
+            state = state,
+            calendarScrollPaged = true,
+            contentHeightMode = ContentHeightMode.Fill,
+            dayContent = dayContent,
+            monthBody = monthBody,
+            monthHeader = monthHeader,
+            monthFooter = monthFooter,
+        )
+    }
+}
+
+@Composable
+private fun MonthHeader(daysOfWeek: List<DayOfWeek>) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .testTag("MonthHeader")
+            .background(colorResource(id = R.color.example_1_bg_secondary))
+            .padding(vertical = 8.dp),
+    ) {
+        for (dayOfWeek in daysOfWeek) {
+            Text(
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center,
+                fontSize = 15.sp,
+                text = dayOfWeek.displayText(),
+            )
+        }
+    }
+}
+//
+
+@Composable
+private fun MonthFooter(selectionCount: Int) {
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .testTag("MonthFooter")
+            .background(colorResource(id = R.color.orange_800))
+            .padding(vertical = 10.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        val text = if (selectionCount == 0) {
+            stringResource(R.string.example_8_zero_selection)
+        } else {
+            pluralStringResource(R.plurals.example_8_selection, selectionCount, selectionCount)
+        }
+        Text(text = text)
+    }
+}
+
+@Composable
+private fun Day(
+    day: CalendarDay,
+    isSelected: Boolean,
+    isToday: Boolean,
+    onClick: (CalendarDay) -> Unit,
+) {
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .clip(RectangleShape)
+            .background(
+                color = when {
+                    isSelected -> colorResource(R.color.example_1_selection_color)
+                    isToday -> colorResource(id = R.color.example_1_white_light)
+                    else -> Color.Transparent
+                },
+            )
+            // Disable clicks on inDates/outDates
+            .clickable(
+                enabled = day.position == DayPosition.MonthDate,
+                showRipple = !isSelected,
+                onClick = { onClick(day) },
+            ),
+        contentAlignment = Alignment.TopStart,//날짜 위치
+    ) {
+        val textColor = when (day.position) {
+            // Color.Unspecified will use the default text color from the current theme
+            DayPosition.MonthDate -> if (isSelected) colorResource(R.color.example_1_bg) else Color.Unspecified
+            DayPosition.InDate, DayPosition.OutDate -> colorResource(R.color.example_1_white_light)
+        }
+        Text(
+            text = day.date.dayOfMonth.toString(),
+            color = textColor,
+            fontSize = 15.sp,
         )
     }
 }
